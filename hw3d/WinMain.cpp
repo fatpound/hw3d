1666c0607a36c3862919f15af9132fda0577deb9
fatpound <fatpound@users.noreply.github.com>
2023-09-22 19:14:38 +0300

    wrapped main code with try catch

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -6,20 +6,37 @@ int CALLBACK WinMain(
     LPSTR     lpCmdLine,
     int       nCmdShow)
 {
-    Window wnd( 640, 480, "hw3dfat" );
-
-    MSG msg;
-    BOOL gResult;
-    while ( ( gResult = GetMessage( &msg, nullptr, 0, 0 ) ) > 0 )
+    try
     {
-        TranslateMessage( &msg );
-        DispatchMessage( &msg );
+        Window wnd(640, 480, "hw3dfat");
+
+        MSG msg;
+        BOOL gResult;
+        while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        if (gResult == -1)
+        {
+            return -1;
+        }
+
+        return msg.wParam;
+    }
+    catch ( const FatException& e )
+    {
+        MessageBox( nullptr, e.what(), e.GetType(), MB_OK | MB_ICONERROR);
+    }
+    catch ( const std::exception& e )
+    {
+        MessageBox( nullptr, e.what(), "Standard Exception", MB_OK | MB_ICONERROR);
+    }
+    catch ( ... )
+    {
+        MessageBox( nullptr, "No Details Available", "Standard Exception", MB_OK | MB_ICONERROR);
     }
 
-    if ( gResult == -1 )
-    {
-        return -1;
-    }
-    
-    return msg.wParam;
+    return -1;
 }
