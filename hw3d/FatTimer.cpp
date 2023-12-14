8c381349270bbdd9a66f33ecc773bf42429b6d3d
fatpound <fatpound@users.noreply.github.com>
2023-12-14 17:41:31 +0300

    added Peek method to FatTimer

--- hw3d/FatTimer.cpp
+++ hw3d/FatTimer.cpp
@@ -1,15 +1,25 @@
 #include "FatTimer.hpp"
 
+using namespace std::chrono;
+
 namespace fatpound::time
 {
+    FatTimer::FatTimer()
+    {
+		last = steady_clock::now();
+    }
+
     float FatTimer::Mark() noexcept
     {
         const auto old = last;
-
         last = steady_clock::now();
 
         const duration<float> frameTime = last - old;
 
         return frameTime.count();
     }
+	float FatTimer::Peek() const noexcept
+	{
+		return duration<float>(steady_clock::now() - last).count();
+	}
 }
--- hw3d/FatTimer.hpp
+++ hw3d/FatTimer.hpp
@@ -2,17 +2,20 @@
 
 #include <chrono>
 
-using namespace std::chrono;
-
 namespace fatpound::time
 {
     class FatTimer
     {
 	public:
+		FatTimer();
+
+
+	public:
         float Mark() noexcept;
+        float Peek() const noexcept;
 
 
     private:
-        steady_clock::time_point last = steady_clock::now();
+		std::chrono::steady_clock::time_point last;
     };
 }
