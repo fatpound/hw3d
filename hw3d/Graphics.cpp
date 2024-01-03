d7167819c7b6a3bd0a414d602e9ca2cfc231f4b4
fatpound <fatpound@users.noreply.github.com>
2024-01-03 18:15:32 +0300

    found the directx normalizer enum  for the color

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -188,7 +188,7 @@ void Graphics::DrawTestTriangle()
     const D3D11_INPUT_ELEMENT_DESC ied[] =
     {
         { "Position", 0, DXGI_FORMAT_R32G32_FLOAT,    0,  0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-        { "Color",    0, DXGI_FORMAT_R8G8B8A8_UINT, 0, 8u, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+        { "Color",    0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, 8u, D3D11_INPUT_PER_VERTEX_DATA, 0 }
     };
     
     GFX_THROW_INFO(pDevice->CreateInputLayout(
