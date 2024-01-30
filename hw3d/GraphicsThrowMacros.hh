10c2cf01588b1c60e3064b1a93edb694c4b78698
fatpound <fatpound@users.noreply.github.com>
2024-01-30 12:01:44 +0300

    fixed a missing character in graphics macros

--- hw3d/GraphicsThrowMacros.hh
+++ hw3d/GraphicsThrowMacros.hh
@@ -9,7 +9,7 @@
 #define GFX_THROW_INFO(hrcall) infoManager_.SetNextIndex(); if (FAILED( hr = (hrcall) )) throw GFX_EXCEPT(hr)
 #define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException( __LINE__, __FILE__, (hr), infoManager_.GetMessages() )
 #define GFX_THROW_INFO_ONLY(call) infoManager_.SetNextIndex(); (call); {auto v = infoManager_.GetMessages(); if(!v.empty()) {throw Graphics::InfoException( __LINE__,__FILE__,v);}}
-#define INFOMAN(gfx) HRESULT hr; DxgiInfoManager& infoManager = GetInfoManager((gfx))
+#define INFOMAN(gfx) HRESULT hr; DxgiInfoManager& infoManager_ = GetInfoManager((gfx))
 
 #else
 
