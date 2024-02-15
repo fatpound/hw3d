65c867a7dccb36750a55cff55af1ebbb4fc1e92b
fatpound <fatpound@users.noreply.github.com>
2024-02-15 11:22:05 +0300

    we will be continuing in a second

--- hw3d/WindowThrowMacros.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define FHWND_EXCEPT(hresult_) Window::HrException(__LINE__, __FILE__, hresult_)
-#define FHWND_LAST_EXCEPT() Window::HrException(__LINE__, __FILE__, GetLastError())
-#define FHWND_NOGFX_EXCEPT() Window::NoGfxException( __LINE__,__FILE__ )
