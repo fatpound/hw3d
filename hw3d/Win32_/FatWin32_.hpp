3f682c65024a6d17502b04c784068a4b214e7a1b
fatpound <84704675+fatpound@users.noreply.github.com>
2024-05-23 15:27:00 +0300

    splitted namespace macros

--- hw3d/Win32_/FatWin32_.hpp
+++ hw3d/Win32_/FatWin32_.hpp
@@ -59,11 +59,4 @@
 #define IN_RELEASE false
 #endif // NDEBUG
 
-#define NAMESPACE_MATH         ::fatpound::math
-#define NAMESPACE_UTIL         ::fatpound::util
-#define NAMESPACE_IO           ::fatpound::win32::io
-#define NAMESPACE_D3D11        ::fatpound::win32::d3d11
-#define NAMESPACE_DIRECTX      ::fatpound::win32::directx
-#define NAMESPACE_DXGI         ::fatpound::win32::dxgi
-#define NAMESPACE_PIPELINE     NAMESPACE_D3D11::pipeline
-#define NAMESPACE_VISUAL       NAMESPACE_D3D11::visual
+#include "FatWin32_Namespaces.hpp"
